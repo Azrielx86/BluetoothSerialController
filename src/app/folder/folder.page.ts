d7ff975440e74f4da30060dcc2fdda0334b481a2
Azrielx86 <edgar1569@outlook.com>
2024-11-06 00:48:31 -0600

    First commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,16 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+  styleUrls: ['./folder.page.scss']
 })
 export class FolderPage implements OnInit {
-  public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  public folder!: string;
   constructor() {}
-
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
---
+++ src/app/typings/BluetoothDevice.ts
@@ -0,0 +1,7 @@
+export interface BluetoothDevice
+{
+  name: string,
+  address: string,
+  id: string,
+  class: number
+}
